9e3fce18fe928f05fd33965a4b82fb6078d7fa41
moonfraction <chandranshsingh@kgpian.iitkgp.ac.in>
2025-01-30 09:44:42 +0530

    long sample works

--- LA3/doencfileserver.c
+++ LA3/doencfileserver.c
@@ -1,256 +1,166 @@
-/*=====================================
+/*
+=====================================
 Assignment 3 Submission
-Name: Chandransh Singh
-Roll number: 22CS30017
-Link of the pcap file:
-=====================================*/
+Name: [Your_Name]
+Roll number: [Your_Roll_Number]
+Link of the pcap file: [Google_Drive_Link_of_the_pcap_File]
+=====================================
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <signal.h>
 
-#define CHUNK_SIZE 60
+#define PORT 8080
+#define BUFFER_SIZE 100
 
-// Global variables
-volatile int active_children = 0;
-
-void print_server_message(const char* ip, int port, const char* message) {
-    printf("[%s:%d] %s \n", 
-           ip ? ip : "SERVER", 
-           port ? port : 20000, 
-           message);
-}
-
-
-// Signal handler for child termination
-void handle_child_exit(int sig) {
-    while (waitpid(-1, NULL, WNOHANG) > 0) {
-        active_children--;
+// Function to encrypt a character using the substitution cipher
+char encrypt_char(char c, const char* key) {
+    if (c >= 'A' && c <= 'Z') {
+        return key[c - 'A'];
+    } else if (c >= 'a' && c <= 'z') {
+        return key[c - 'a'] + ('a' - 'A');
     }
-    
-    if (active_children == 0) {
-        char choice;
-        printf("\nAll clients disconnected. Wait for new clients? (y/n): ");
-        scanf(" %c", &choice);
-        
-        if (choice == 'n' || choice == 'N') {
-            printf("Server shutting down...\n");
-            exit(0);
-        }
-        printf("Waiting for new connections...\n");
-    }
-}
-
-void handle_client_disconnect(int sockfd, struct sockaddr_in *peer_addr) {
-    char message[100];
-    snprintf(message, sizeof(message), "Client disconnected unexpectedly");
-    print_server_message(inet_ntoa(peer_addr->sin_addr), 
-                        ntohs(peer_addr->sin_port), 
-                        message);
-}
-
-
-// Function to encrypt the file using substitution cipher
-void encrypt_file(const char* input_file, const char* output_file, const char* key) {
-    FILE *fin = fopen(input_file, "r");
-    FILE *fout = fopen(output_file, "w");
-    int c;
-    
-    while ((c = fgetc(fin)) != EOF) {
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-            int idx = (c >= 'a') ? (c - 'a') : (c - 'A');
-            char mapped = key[idx];
-            fputc((c >= 'a') ? (mapped + 32) : mapped, fout);
-        } else {
-            fputc(c, fout);
-        }
-    }
-    
-    fclose(fin);
-    fclose(fout);
+    return c;  // Return unchanged if not a letter
 }
 
 int main() {
-    // Register signal handler for child termination
-    signal(SIGCHLD, handle_child_exit);
-
-    int sockfd, newsockfd;
-    struct sockaddr_in serv_addr, cli_addr;
-    char buffer[CHUNK_SIZE];
-    socklen_t clilen;
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char key[27] = {0};
     
-    // Create socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Cannot create socket");
-        exit(0);
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
-
-    // Set socket options to reuse address
-    // to avoid "Address already in use" error while binding
-    int yes = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    
+    // Set socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     
-    // Initialize server address structure
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(20000);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
     
-    // Bind socket
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Unable to bind local address");
-        exit(0);
+    // Bind socket to address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
     
     // Listen for connections
-    // up to 5 concurrent client requests will be queued up
-    listen(sockfd, 5);
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
     
-    // Print server start message
-    print_server_message("SERVER", 20000, "Server started. Listening for connections");
-
-    
+    printf("Server is listening on port %d...\n", PORT);
     
     while (1) {
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-        
-        if (newsockfd < 0) {
-            perror("Accept error");
-            continue;
+        // Accept incoming connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
         }
-
-        if(fork()==0){
-
-            // Close the old socket since all communications will be through the new socket
-            close(sockfd);
-
-            // Get client information
-            struct sockaddr_in peer_addr;
-            socklen_t peer_len = sizeof(peer_addr);
-            getpeername(newsockfd, (struct sockaddr*)&peer_addr, &peer_len);
-
-            // Set up signal handler for SIGPIPE
-            signal(SIGPIPE, SIG_IGN);
-
-            // Print connection info
-            print_server_message(inet_ntoa(peer_addr.sin_addr), 
-                    ntohs(peer_addr.sin_port), 
-                    "New client connected");
-
+        
+        // Get client IP and port
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(address.sin_addr), client_ip, INET_ADDRSTRLEN);
+        int client_port = ntohs(address.sin_port);
+        
+        while (1) {
+            // First receive the key
+            memset(key, 0, sizeof(key));
+            int key_bytes = recv(new_socket, key, 26, 0);
+            if (key_bytes <= 0) break;
+            key[26] = '\0';
             
+            // Create temporary file name using client IP and port
+            char temp_filename[100];
+            snprintf(temp_filename, sizeof(temp_filename), "%s.%d.txt", client_ip, client_port);
+            
+            // Open temporary file for writing
+            FILE *temp_file = fopen(temp_filename, "w");
+            if (!temp_file) {
+                perror("Error creating temporary file");
+                continue;
+            }
+            
+            // Receive file content in chunks
             while (1) {
-                // Receive the key first
-                char key[27];
-                memset(key, 0, sizeof(key));
-                // Add error checking for send/recv operations
-                if (recv(newsockfd, key, 26, 0) <= 0) {
-                    handle_client_disconnect(newsockfd, &peer_addr);
-                    break;
-                }
-
-                // File transfer messages
-                char msg[256];
-
-                // Print received key info
-                snprintf(msg, sizeof(msg), "Received Key for encryption of file: %s", key);
-                print_server_message(inet_ntoa(peer_addr.sin_addr), 
-                    ntohs(peer_addr.sin_port), 
-                    msg);
+                memset(buffer, 0, BUFFER_SIZE);
+                int bytes_received = recv(new_socket, buffer, BUFFER_SIZE - 1, 0);
+                if (bytes_received <= 0) break;
                 
-                // temporary filename using client's IP and port
-                char temp_filename[100];
-                snprintf(temp_filename, sizeof(temp_filename), "%s.%d.txt", 
-                        inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+                fwrite(buffer, 1, bytes_received, temp_file);
                 
-                // Open temporary file for writing
-                FILE *temp_file = fopen(temp_filename, "w");
-
-                // Track total bytes received
-                long total_bytes = 0;
-                int chunk_count = 0;
-                
-                // Receive file content in chunks
-                int bytes_received;
-                while ((bytes_received = recv(newsockfd, buffer, CHUNK_SIZE - 1, 0)) > 0) {
-                    buffer[bytes_received] = '\0';
-                    chunk_count++;
-                    total_bytes += bytes_received;
-                    
-                    
-                    snprintf(msg, sizeof(msg), "Received chunk %d: %d bytes", chunk_count, bytes_received);
-                    print_server_message(inet_ntoa(peer_addr.sin_addr), 
-                        ntohs(peer_addr.sin_port), 
-                        msg);
-
-                    printf("+++ Chunk %d content:\n%s\n", chunk_count, buffer);
-                    if (bytes_received < CHUNK_SIZE - 1) {  // Last chunk
-                        fputs(buffer, temp_file);
-                        break;
-                    }
-                    fputs(buffer, temp_file);
-                }
-
-                snprintf(msg, sizeof(msg), "File transfer complete. Total: %ld bytes in %d chunks", 
-                         total_bytes, chunk_count);
-                print_server_message(inet_ntoa(peer_addr.sin_addr), 
-                                    ntohs(peer_addr.sin_port), 
-                                    msg);
-
-                fclose(temp_file);
-                
-                // Create encrypted filename
-                char enc_filename[120];
-                snprintf(enc_filename, sizeof(enc_filename), "%s.enc", temp_filename);
-                
-                // Encrypt the file
-                encrypt_file(temp_filename, enc_filename, key);
-                
-                // Send encrypted file back to client
-                FILE *enc_file = fopen(enc_filename, "r");
-                size_t bytes_read;
-                while ((bytes_read = fread(buffer, 1, CHUNK_SIZE - 1, enc_file)) > 0) {
-                    send(newsockfd, buffer, bytes_read, 0);
-                    if (bytes_read < CHUNK_SIZE - 1) {  // Last chunk
-                        break;
-                    }
-                }
-                fclose(enc_file);
-                
-                // Remove temporary files
-                // remove(temp_filename);
-                // remove(enc_filename);
-                
-                // Check if client wants to continue
-                memset(buffer, 0, CHUNK_SIZE);
-                if (recv(newsockfd, buffer, CHUNK_SIZE - 1, 0) <= 0) break;
-                if (strcmp(buffer, "No") == 0) {
-                    // Print disconnection info using print_server_message function
-                    print_server_message(inet_ntoa(peer_addr.sin_addr),
-                                      ntohs(peer_addr.sin_port),
-                                      "Client disconnected");
-                    break;
+                // Check if this is the end of the file
+                if (bytes_received < BUFFER_SIZE - 1) break;
+            }
+            fclose(temp_file);
+            
+            // Create encrypted file name
+            char enc_filename[120];
+            snprintf(enc_filename, sizeof(enc_filename), "%s.enc", temp_filename);
+            
+            // Open files for encryption
+            FILE *input = fopen(temp_filename, "r");
+            FILE *output = fopen(enc_filename, "w");
+            
+            if (!input || !output) {
+                perror("Error opening files for encryption");
+                if (input) fclose(input);
+                if (output) fclose(output);
+                continue;
+            }
+            
+            // Perform encryption
+            int c;
+            while ((c = fgetc(input)) != EOF) {
+                fputc(encrypt_char(c, key), output);
+            }
+            
+            fclose(input);
+            fclose(output);
+            
+            // Send encrypted file back to client
+            FILE *enc_file = fopen(enc_filename, "r");
+            if (!enc_file) {
+                perror("Error opening encrypted file");
+                continue;
+            }
+            
+            while (!feof(enc_file)) {
+                size_t bytes_read = fread(buffer, 1, BUFFER_SIZE - 1, enc_file);
+                if (bytes_read > 0) {
+                    send(new_socket, buffer, bytes_read, 0);
                 }
             }
             
-            close(newsockfd);
-            exit(0);
-        }
-        else{
-            active_children++;
-            close(newsockfd);
+            fclose(enc_file);
+            
+            // Wait for client's next request
+            memset(buffer, 0, BUFFER_SIZE);
+            int confirmation = recv(new_socket, buffer, BUFFER_SIZE - 1, 0);
+            if (confirmation <= 0 || strcasecmp(buffer, "No") == 0) {
+                break;
+            }
         }
         
+        close(new_socket);
+        printf("Connection with client closed\n");
     }
     
-
     return 0;
 }
--- LA3/retrieveencfileclient.c
+++ LA3/retrieveencfileclient.c
@@ -1,127 +1,183 @@
-/*=====================================
+/*
+=====================================
 Assignment 3 Submission
-Name: Chandransh Singh
-Roll number: 22CS30017
-Link of the pcap file:
-=====================================*/
+Name: [Your_Name]
+Roll number: [Your_Roll_Number]
+Link of the pcap file: [Google_Drive_Link_of_the_pcap_File]
+=====================================
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define CHUNK_SIZE 80
+#define PORT 8080
+#define BUFFER_SIZE 100
 
 int main() {
-    int sockfd;
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char buffer[CHUNK_SIZE];
+    char buffer[BUFFER_SIZE] = {0};
+    char filename[100];
+    char key[27];
+    char continue_encryption[10];
     
     // Create socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Unable to create socket");
-        exit(0);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
     }
     
-    // Initialize server address structure
     serv_addr.sin_family = AF_INET;
-    inet_aton("127.0.0.1", &serv_addr.sin_addr);
-    serv_addr.sin_port = htons(20000);
+    serv_addr.sin_port = htons(PORT);
+    
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
     
     // Connect to server
-    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Unable to connect to server");
-        exit(0);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
     }
     
     while (1) {
-        char filename[100];
-        char key[27];
-        FILE *fp;
+        // Clear buffers
+        memset(filename, 0, sizeof(filename));
+        memset(key, 0, sizeof(key));
+        memset(buffer, 0, sizeof(buffer));
         
-        // Getting filename from user
+        // Get filename from user
         while (1) {
-            printf("--> Enter the filename to encrypt: ");
-            scanf("%s", filename);
-            
-            fp = fopen(filename, "r");
-            if (fp == NULL) {
-                printf("*** NOTFOUND %s\n", filename);
+            printf("Enter the filename to encrypt: ");
+            if (scanf("%99s", filename) != 1) {
+                printf("Error reading filename\n");
+                while (getchar() != '\n'); // Clear input buffer
                 continue;
             }
+            
+            // Check if file exists
+            FILE *file = fopen(filename, "r");
+            if (!file) {
+                printf("NOTFOUND %s\n", filename);
+                continue;
+            }
+            fclose(file);
             break;
         }
         
-        // Getting encryption key from user
+        // Clear input buffer
+        while (getchar() != '\n');
+        
+        // Get encryption key from user
         while (1) {
-            printf("--> Enter the 26-character encryption key: ");
-            scanf("%s", key);
-            
-            if (strlen(key) != 26) {
-                printf("### Error: Key must be exactly 26 characters long\n");
+            printf("Enter the 26-character encryption key: ");
+            if (fgets(key, sizeof(key), stdin) == NULL) {
+                printf("Error reading key\n");
                 continue;
             }
-            // cheking if key is valid - should not contain duplicate ascii characters
-            int ascii[256] = {0};
-            for (int i = 0; i < 26; i++) {
-                if (ascii[(unsigned char)key[i]] == 1) {
-                    printf("*** Warning: Key should not contain duplicate characters\n");
-                    printf("*** You may not be able to decrypt the file later\n");
-                    break;
-                }
-                ascii[(unsigned char)key[i]] = 1;
+            
+            // Remove newline if present
+            key[strcspn(key, "\n")] = 0;
+            
+            if (strlen(key) != 26) {
+                printf("Error: Key must be exactly 26 characters (got %lu characters)\n", strlen(key));
+                continue;
             }
             break;
         }
         
         // Send key to server
-        send(sockfd, key, 26, 0);
-        
-        // Send file content to server in chunks
-        size_t bytes_read;
-        while ((bytes_read = fread(buffer, 1, CHUNK_SIZE - 1, fp)) > 0) {
-            send(sockfd, buffer, bytes_read, 0);
+        if (send(sock, key, 26, 0) != 26) {
+            printf("Error sending key to server\n");
+            break;
         }
-        fclose(fp);
+        
+        // Send file content to server
+        FILE *input_file = fopen(filename, "r");
+        if (!input_file) {
+            printf("Error opening input file\n");
+            break;
+        }
+        
+        size_t bytes_read;
+        while ((bytes_read = fread(buffer, 1, BUFFER_SIZE - 1, input_file)) > 0) {
+            if (send(sock, buffer, bytes_read, 0) != bytes_read) {
+                printf("Error sending file content\n");
+                fclose(input_file);
+                goto cleanup;
+            }
+            memset(buffer, 0, BUFFER_SIZE);
+        }
+        fclose(input_file);
+        
+        // Small delay to ensure server processes the file
+        usleep(100000);  // 100ms delay
         
         // Create encrypted filename
         char enc_filename[120];
         snprintf(enc_filename, sizeof(enc_filename), "%s.enc", filename);
         
         // Receive encrypted file from server
-        FILE *enc_file = fopen(enc_filename, "w");
-        int bytes_received;
+        FILE *output_file = fopen(enc_filename, "w");
+        if (!output_file) {
+            printf("Error creating output file\n");
+            break;
+        }
         
-        while ((bytes_received = recv(sockfd, buffer, CHUNK_SIZE - 1, 0)) > 0) {
-            buffer[bytes_received] = '\0';
-            fputs(buffer, enc_file);
-            if (bytes_received < CHUNK_SIZE - 1) {  // Last chunk
+        memset(buffer, 0, BUFFER_SIZE);
+        while ((bytes_read = recv(sock, buffer, BUFFER_SIZE - 1, 0)) > 0) {
+            if (fwrite(buffer, 1, bytes_read, output_file) != bytes_read) {
+                printf("Error writing to output file\n");
+                fclose(output_file);
+                goto cleanup;
+            }
+            memset(buffer, 0, BUFFER_SIZE);
+            
+            // If we received less than buffer size, this might be the end
+            if (bytes_read < BUFFER_SIZE - 1) {
                 break;
             }
         }
-        fclose(enc_file);
+        fclose(output_file);
         
-        printf("+++ File encrypted successfully!\n");
-        printf("+++ Original file: %s\n", filename);
-        printf("+++ Encrypted file: %s\n", enc_filename);
+        printf("File encrypted successfully!\n");
+        printf("Original file: %s\n", filename);
+        printf("Encrypted file: %s\n", enc_filename);
         
         // Ask user if they want to encrypt another file
-        char continue_choice[10];
-        printf("\n--> Do you want to encrypt another file? (Yes/No): ");
-        scanf("%s", continue_choice);
+        while (1) {
+            printf("Do you want to encrypt another file? (Yes/No): ");
+            if (scanf("%9s", continue_encryption) != 1) {
+                printf("Error reading input\n");
+                while (getchar() != '\n'); // Clear input buffer
+                continue;
+            }
+            while (getchar() != '\n'); // Clear input buffer
+            
+            if (strcasecmp(continue_encryption, "Yes") == 0 || strcasecmp(continue_encryption, "No") == 0) {
+                break;
+            }
+            printf("Please enter either 'Yes' or 'No'\n");
+        }
         
-        // Send choice to server
-        send(sockfd, continue_choice, strlen(continue_choice) + 1, 0);
+        // Send response to server
+        if (send(sock, continue_encryption, strlen(continue_encryption), 0) <= 0) {
+            printf("Error sending continue response\n");
+            break;
+        }
         
-        if (strcmp(continue_choice, "No") == 0) {
+        if (strcasecmp(continue_encryption, "No") == 0) {
             break;
         }
     }
     
-    close(sockfd);
+cleanup:
+    close(sock);
     return 0;
 }
